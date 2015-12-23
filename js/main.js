d84a8c900d9c5710e4cd61b132774b2effc2d84d
unknown <vmuler@KIEMWORVMULER.matone.materialise>
2015-12-23 18:06:46 +0200

    slider added, corner partly is done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vmuler on 23.12.2015.
+ */
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        minSlides: 3,
+        maxSlides: 6,
+        slideWidth: 400,
+        slideMargin: 100
+    });
+});
