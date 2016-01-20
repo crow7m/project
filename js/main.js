d44e9f8b13a2cc018fc3c619addd4f0b284b52cd
crow7m <crow7m@gmail.com>
2016-01-20 18:23:56 +0200

    home.html started

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ $(document).ready(function(){
         slideMargin: 100
     });
 });
+
+   $(document).ready(function(){ $('.logo-left, .text1').hover(function () {
+        $('.test-logo').css('backgroundPosition', '0 -177px');
+    }, function () {
+        $('.test-logo').css('backgroundPosition', '0 0');
+    });
+    $('.logo-right, .text2').hover(function () {
+        $('.test-logo').css('backgroundPosition', '0 -354px');
+    }, function () {
+        $('.test-logo').css('backgroundPosition', '0 0');
+    });
+       });
