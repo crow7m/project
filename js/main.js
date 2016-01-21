83feddb95bf148aa147802e8640ce9468892f5fe
Marina Zadoyanchuk <marinazadoyanchuk@Marinas-MacBook-Pro.local>
2016-01-21 19:21:53 +0200

    new structure

--- js/main.js
+++ js/main.js
@@ -25,15 +25,3 @@ $(document).ready(function () {
    $('.text2').css('color', 'black');
   });
  });
- /*
-   $(document).ready(function(){ $('.logo-left, .text1').hover(function () {
-        $('.test-logo').css('backgroundPosition', '0 -177px');
-    }, function () {
-        $('.test-logo').css('backgroundPosition', '0 0');
-    });
-    $('.logo-right, .text2').hover(function () {
-        $('.test-logo').css('backgroundPosition', '0 -354px');
-    }, function () {
-        $('.test-logo').css('backgroundPosition', '0 0');
-    });
-  });*/
